f42572735edf8c4cbe1daa5fe8e2808331d2e9dc
Tikeenters <142219021+Tikeenters@users.noreply.github.com>
2026-04-05 00:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PyPDF2 import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
+from langchain_classic.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 import os
 import glob # This new tool helps us find multiple files
