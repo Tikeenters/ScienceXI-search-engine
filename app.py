0920ef5a2ee1009aacffb93fc2e8eee5854e8b15
Tikeenters <142219021+Tikeenters@users.noreply.github.com>
2026-04-05 15:19:47 +0530

    Changed gemini from flash 1.5 to flash
    model = ChatGoogleGenerativeAI(model="gemini-flash-latest", temperature=0.3)

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def get_conversational_chain():
     Question: \n{question}\n
     Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-flash-latest", temperature=0.3)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
